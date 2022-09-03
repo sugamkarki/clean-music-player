064453bdd490d904534a6fcdb22b662c9b64c720
Sugam Karki <sugamkarki7058@gmail.com>
2022-09-03 22:25:03 +0545

    fixed layout and added default songs

---
+++ components/audios.ts
@@ -0,0 +1,14 @@
+export const songsdata = [
+  {
+    title: "Tuyo--Narcos",
+    url: "/songs/Tuyo.mp3",
+  },
+  {
+    title: "Dancing with Myself--PostModern Jukebox",
+    url: "/songs/DancingWithMyself.mp3",
+  },
+  {
+    title: "Royals",
+    url: "/songs/Royals.mp3",
+  },
+];
